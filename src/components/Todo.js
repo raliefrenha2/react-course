e22a6caa8b5f145142dc5c4946bc5d9f5669f8d0
Romi Alief Rahman <raliefrenha2@gmail.com>
2021-04-02 17:23:53 +0700

    Handling Events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+  function deleteHandler() {
+    console.log("clicked");
+    console.log(props.title);
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
